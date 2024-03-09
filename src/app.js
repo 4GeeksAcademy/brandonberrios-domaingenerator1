8b1bb2e273bca97b03e877e842b278ba5a21c549
Brandon Berrios <140827644+brandonberrios@users.noreply.github.com>
2024-03-09 04:52:35 +0000

    Update push force

--- src/app.js
+++ src/app.js
@@ -2,34 +2,27 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  let articles = ["the", "our"];
-  let adj = ["great", "big"];
-  let noun = ["jogger", "racoon"];
+  let pronouns = ["the", "our", "that", "my"];
+  let adjs = ["big", "cool", "pretty", "small", "awesome"];
+  let nouns = ["planet", "lion", "jogger", "house"];
   let tlds = [".com", ".net", ".org"];
 
-  let domainNames = [];
+  let domainName = [];
 
-  for (let articles of articles) {
-    for (let adj of adj) {
-      for (let noun of noun) {
-        for (let tlds of tlds) {
-          console.log(`${articles}${adj}${noun}${tlds}`);
+  for (let pronoun of pronouns) {
+    for (let adj of adjs) {
+      for (let noun of nouns) {
+        for (let tld of tlds) {
+          domainName.push(`${pronoun}${adj}${noun}${tld}`);
         }
       }
     }
-  }
 
-  document.body.innerHTML = (
-    <ul>
-      $
-      {domainNames.map(domainNames => (
-        <li>${domainNames}</li>
-      ))}
-    </ul>
-  );
+    document.getElementById("domainGenerator").onclick = () => {
+      document.getElementById("generate").innerHTML = `<h1>${
+        domainName[Math.floor(Math.random() * domainName.length)]
+      }</h1>`;
+    };
+  }
 };
